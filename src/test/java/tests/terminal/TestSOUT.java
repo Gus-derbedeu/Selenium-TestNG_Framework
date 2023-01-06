ad60d6ad4323f7fda84071f605ca95692870189d
GUS\Vladimir <atis-magnus@mail.ru>
2023-01-06 13:18:39 +0200

    Additional testNG implementations, working with 2+ test suits

---
+++ src/test/java/tests/terminal/TestSOUT.java
@@ -0,0 +1,11 @@
+package tests.terminal;
+
+import org.testng.annotations.Test;
+
+public class TestSOUT {
+
+    @Test
+    public void testSOUT(){
+        System.out.println("Test terminal");
+    }
+}
