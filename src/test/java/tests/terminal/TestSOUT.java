d9dc2083697f11782c8ff886629d2d9ff3c549d0
GUS\Vladimir <atis-magnus@mail.ru>
2023-01-07 18:22:44 +0200

    DataProvider implementations

--- src/test/java/tests/terminal/TestSOUT.java
+++ src/test/java/tests/terminal/TestSOUT.java
@@ -1,11 +1,14 @@
 package tests.terminal;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import tests.filter.DataProviderClass;
 
 public class TestSOUT {
 
-    @Test
-    public void testSOUT(){
-        System.out.println("Test terminal");
+    @Test (dataProvider = "dataProviderMethod", dataProviderClass = DataProviderClass.class)
+    public void testSOUT(String number, String string){
+
+        System.out.println("Test terminal: " + number + string);
     }
 }
